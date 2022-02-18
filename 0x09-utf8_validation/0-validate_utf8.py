1d333dee10b8c4247ba75f230a8a726c1f418183
oimoralest <oimoralest@gmail.com>
2022-02-17 19:23:50 -0500

    Added task

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+#!usr/bin/python3
+"""This script defines validUTF8 function"""
+
+def validUTF8(data):
+    """ Checks if a given data set represents a valid UTF-8 encoding """
+    c = 0
+    for n in data:
+        m = 0b10000000
+        if not c:
+            while (m & n):
+                c += 1
+                m >>= 1
+            if c > 4:
+                return False
+            if c:
+                c -= 1
+                if c == 0:
+                    return False
+        elif c > 0:
+            if n >> 6 != 2:
+                return False
+            c -= 1
+    return not c
