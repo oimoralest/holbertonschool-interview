84b0e1cc3fd993bbf4477c11208d0151d21c4778
oimoralest <oimoralest@gmail.com>
2022-02-24 19:29:30 -0500

    Slide line interview challenge
        Fixed header file
    
    [project: 0x0A-slide_line]

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,9 +1,10 @@
-#ifdef SLIDE_LINE_H
+#ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 #define SLIDE_LEFT 1
 #define SLIDE_RIGHT 2
