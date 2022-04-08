b5c94127258277ed065628e0cdf192519c732042
oimoralest <oimoralest@gmail.com>
2022-04-07 19:17:37 -0500

    Reformated

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,24 +6,24 @@
 const request = require('request');
 
 const makeGetRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        request
-        .get(url, (err, response, body) => {
-            if (err || response.statusCode !== 200) {
-            reject(err);
-            } else {
-            resolve(JSON.parse(body));
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    request
+      .get(url, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(body));
+        }
+      });
+  });
 };
 
-async function main () {
-    const result = await makeGetRequest(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`);
-    for (const character of result.characters) {
-        const { name } = await makeGetRequest(character);
-        console.log(name);
-    }
+async function main() {
+  const result = await makeGetRequest(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`);
+  for (const character of result.characters) {
+    const { name } = await makeGetRequest(character);
+    console.log(name);
+  }
 }
 
 main();
